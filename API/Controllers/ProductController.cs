2072fe4236c85a31ab2c73469ca2a4a40eadcd8b
Franco Ceballos <francomanuelceballos@gmail.com>
2024-09-18 12:13:51 -0300

    Added specifications

--- API/Controllers/ProductController.cs
+++ API/Controllers/ProductController.cs
@@ -14,25 +14,46 @@ namespace Core.Controllers
     {
 
         private readonly IProductRepository _productRepository;
+        private readonly IGenericRepository<ProductBrand> _productBrandRepo;
+        private readonly IGenericRepository<ProductType> _productTypeRepo;
+        private readonly IGenericRepository<Product> _productRepo;
 
-        public ProductController(IProductRepository productRepository)
+        public ProductController(IProductRepository productRepository, 
+            IGenericRepository<ProductBrand> productBrandRepo, 
+            IGenericRepository<ProductType> productTypeRepo, 
+            IGenericRepository<Product> productRepo)
         {
             _productRepository = productRepository;
+            _productBrandRepo = productBrandRepo;
+            _productTypeRepo = productTypeRepo;
+            _productRepo = productRepo;
+            
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<ProductController>>> GetProducts()
+        public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await _productRepository.GetProductsAsync();
+            var products = await _productRepo.ListAllAsync();
             return Ok(products);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         { 
-            var product = await _productRepository.GetProductByIdAsync(id);
+            var product = await _productRepo.GetByIdAsync(id);
             return Ok(product);
         }
 
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+        {
+            return Ok(await _productTypeRepo.ListAllAsync());
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            return Ok(await _productBrandRepo.ListAllAsync());
+        }
     }
 }
